3195e20f1e319f7fa259f4b51a5d84694d006add
Milt Reder <milt@yetanalytics.com>
2017-06-22 11:11:07 -0400

    updated deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/yetanalytics/re-thread"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
-                 [org.clojure/clojurescript "1.9.293"]
-                 [reagent "0.6.0"]
-                 [re-frame "0.9.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojurescript "1.9.562"]
+                 [reagent "0.6.2"]
+                 [re-frame "0.9.4"]
                  [com.cognitect/transit-cljs "0.8.239"]])
