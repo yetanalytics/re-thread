5fb40224d809d82ba5011fefee34b95f22d72454
Milt Reder <milt@yetanalytics.com>
2017-10-08 02:15:31 -0400

    v0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.yetanalytics/re-thread "0.1.3-SNAPSHOT"
+(defproject com.yetanalytics/re-thread "0.1.3"
   :description "Communicate with re-frame in a Web Worker"
   :url "https://github.com/yetanalytics/re-thread"
   :license {:name "Eclipse Public License"
